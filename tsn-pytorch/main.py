3acbef9ab4c2292a4ea36f34d0ffbf63494f8d3f
Sucre <471607065@qq.com>
2019-03-12 16:49:04 +0800

    Add files via upload

---
+++ tsn-pytorch/main.py
@@ -0,0 +1,348 @@
+import argparse
+import os
+import time
+import shutil
+import torch
+import torchvision
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+from torch.nn.utils import clip_grad_norm
+
+from dataset import TSNDataSet
+from models import TSN
+from transforms import *
+from opts import parser
+import numpy as np
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+best_prec1 = 0
+
+
+def main():
+    global args, best_prec1
+    args = parser.parse_args()
+
+    if args.dataset == 'ucf101':
+        num_class = 101
+    elif args.dataset == 'hmdb51':
+        num_class = 51
+    elif args.dataset == 'kinetics':
+        num_class = 400
+    elif args.dataset == 'myDataset':
+        num_class = 12
+    else:
+        raise ValueError('Unknown dataset '+args.dataset)
+
+    model = TSN(num_class, args.num_segments, args.modality,
+                base_model=args.arch,
+                consensus_type=args.consensus_type, dropout=args.dropout, partial_bn=not args.no_partialbn)
+
+    crop_size = model.crop_size
+    scale_size = model.scale_size
+    input_mean = model.input_mean
+    input_std = model.input_std
+    policies = model.get_optim_policies()
+    train_augmentation = model.get_augmentation()
+
+    model = torch.nn.DataParallel(model, device_ids=args.gpus).cuda()
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print(("=> loading checkpoint '{}'".format(args.resume)))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            print(("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.evaluate, checkpoint['epoch'])))
+        else:
+            print(("=> no checkpoint found at '{}'".format(args.resume)))
+
+    cudnn.benchmark = True
+
+    # Data loading code
+    if args.modality != 'RGBDiff':
+        normalize = GroupNormalize(input_mean, input_std)
+    else:
+        normalize = IdentityTransform()
+
+    if args.modality == 'RGB':
+        data_length = 1
+    elif args.modality in ['Flow', 'RGBDiff']:
+        data_length = 5
+
+    train_loader = torch.utils.data.DataLoader(
+        TSNDataSet("", args.train_list, num_segments=args.num_segments,
+                   new_length=data_length,
+                   modality=args.modality,
+                   image_tmpl="img_{:05d}.jpg" if args.modality in ["RGB", "RGBDiff"] else args.flow_prefix+"{}_{:05d}.jpg",
+                   transform=torchvision.transforms.Compose([
+                       train_augmentation,
+                       Stack(roll=args.arch == 'BNInception'),
+                       ToTorchFormatTensor(div=args.arch != 'BNInception'),
+                       normalize,
+                   ])),
+        batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        TSNDataSet("", args.val_list, num_segments=args.num_segments,
+                   new_length=data_length,
+                   modality=args.modality,
+                   image_tmpl="img_{:05d}.jpg" if args.modality in ["RGB", "RGBDiff"] else args.flow_prefix+"{}_{:05d}.jpg",
+                   random_shift=False,
+                   transform=torchvision.transforms.Compose([
+                       GroupScale(int(scale_size)),
+                       GroupCenterCrop(crop_size),
+                       Stack(roll=args.arch == 'BNInception'),
+                       ToTorchFormatTensor(div=args.arch != 'BNInception'),
+                       normalize,
+                   ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    # define loss function (criterion) and optimizer
+    if args.loss_type == 'nll':
+        criterion = torch.nn.CrossEntropyLoss().cuda()
+    else:
+        raise ValueError("Unknown loss type")
+
+    for group in policies:
+        print(('group: {} has {} params, lr_mult: {}, decay_mult: {}'.format(
+            group['name'], len(group['params']), group['lr_mult'], group['decay_mult'])))
+
+    optimizer = torch.optim.SGD(policies,
+                                args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, 0)
+        return
+
+    f, axs = plt.subplots(4, 1, figsize=(10, 5))
+    if args.start_epoch==0:
+        train_acc = []
+        train_loss = []
+        val_acc = []
+        val_loss = []
+        epochs = []
+        val_epochs = []
+    else:
+        train_acc = np.load("./%s/train_acc.npy" % args.snapshot_pref).tolist()
+        train_loss = np.load("./%s/train_loss.npy" % args.snapshot_pref).tolist()
+        val_acc = np.load("./%s/val_acc.npy" % args.snapshot_pref).tolist()
+        val_loss = np.load("./%s/val_loss.npy" % args.snapshot_pref).tolist()
+        epochs = np.load("./%s/epochs.npy" % args.snapshot_pref).tolist()
+        val_epochs = np.load("./%s/val_epochs.npy" % args.snapshot_pref).tolist()
+    for epoch in range(args.start_epoch, args.epochs):
+        adjust_learning_rate(optimizer, epoch, args.lr_steps)
+
+        # train for one epoch
+        acc, loss = train(train_loader, model, criterion, optimizer, epoch)
+        train_acc.append(acc)
+        train_loss.append(loss)
+        epochs.append(epoch)
+        # evaluate on validation set
+        if (epoch + 1) % args.eval_freq == 0 or epoch == args.epochs - 1:
+            prec1, v_loss = validate(val_loader, model, criterion, (epoch + 1) * len(train_loader))
+
+            # remember best prec@1 and save checkpoint
+            is_best = prec1 > best_prec1
+            best_prec1 = max(prec1, best_prec1)
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'arch': args.arch,
+                'state_dict': model.state_dict(),
+                'best_prec1': best_prec1,
+            }, is_best)
+            val_acc.append(prec1)
+            val_loss.append(v_loss)
+            val_epochs.append(epoch)
+        axs[0].plot(val_epochs, val_loss, c='b', marker='.', label='val_loss')
+        axs[1].plot(val_epochs, val_acc, c='r', marker='.', label='val_acc')
+        axs[2].plot(epochs, train_loss, c='b', marker='.', label='train_loss')
+        axs[3].plot(epochs, train_acc, c='r', marker='.', label='train_acc')
+        plt.title('TSN_' + args.snapshot_pref)
+        if epoch == 0:
+            for i in range(4):
+                axs[i].legend(loc='best')
+        plt.pause(0.000001)
+        if not os.path.exists(args.snapshot_pref):
+            os.makedirs(args.snapshot_pref)
+        plt.savefig('./%s/%s.jpg' % (args.snapshot_pref, str(epoch).zfill(5)))
+    np.save("./%s/train_acc.npy" % args.snapshot_pref, train_acc)
+    np.save("./%s/train_loss.npy" % args.snapshot_pref, train_loss)
+    np.save("./%s/val_acc.npy" % args.snapshot_pref, val_acc)
+    np.save("./%s/val_loss.npy" % args.snapshot_pref, val_loss)
+    np.save("./%s/val_epochs.npy" % args.snapshot_pref, val_epochs)
+    np.save("./%s/epochs.npy" % args.snapshot_pref, epochs)
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    if args.no_partialbn:
+        model.module.partialBN(False)
+    else:
+        model.module.partialBN(True)
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    acc_list = []
+    loss_list = []
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1,5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        if args.clip_gradient is not None:
+            total_norm = clip_grad_norm(model.parameters(), args.clip_gradient)
+            if total_norm > args.clip_gradient:
+                print("clipping gradient: {} with coef {}".format(total_norm, args.clip_gradient / total_norm))
+
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print(('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5, lr=optimizer.param_groups[-1]['lr'])))
+        acc_list.append(top1.avg)
+        loss_list.append(losses.avg)
+    return np.mean(acc_list), np.mean(loss_list)
+
+
+def validate(val_loader, model, criterion, iter, logger=None):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input, volatile=True)
+        target_var = torch.autograd.Variable(target, volatile=True)
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1,5))
+
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print(('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1, top5=top5)))
+
+    print(('Testing Results: Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f} Loss {loss.avg:.5f}'
+          .format(top1=top1, top5=top5, loss=losses)))
+
+    return top1.avg, losses.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    filename = '_'.join((args.snapshot_pref, args.modality.lower(), filename))
+    torch.save(state, filename)
+    if is_best:
+        best_name = '_'.join((args.snapshot_pref, args.modality.lower(), 'model_best.pth.tar'))
+        shutil.copyfile(filename, best_name)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch, lr_steps):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    decay = 0.1 ** (sum(epoch >= np.array(lr_steps)))
+    lr = args.lr * decay
+    decay = args.weight_decay
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr * param_group['lr_mult']
+        param_group['weight_decay'] = decay * param_group['decay_mult']
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+if __name__ == '__main__':
+    main()
