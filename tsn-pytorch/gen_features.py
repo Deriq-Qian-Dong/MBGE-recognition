1b2dde15d5b7b8c9d37e25c3cb6346f9b4dee4d6
Sucre <qian.dong.2018@gmail.com>
2019-03-14 14:22:50 +0800

    update

--- tsn-pytorch/gen_features.py
+++ tsn-pytorch/gen_features.py
@@ -5,6 +5,7 @@ Created on 2019/01/11 14:08
 @email: qian.dong.2018@gmail.com
 """
 import warnings
+
 warnings.filterwarnings("ignore")
 import argparse
 import os
@@ -12,22 +13,26 @@ from dataset import TSNDataSet
 from models import TSN
 from transforms import *
 import matplotlib
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from optparse import OptionParser
 
 optParser = OptionParser()
-optParser.add_option('-t', '--train_list', action='store', type="string", dest='train_list',
-                     help="train list's path", default="../tools/train_val_list/cs_train_list.txt")
-optParser.add_option('-o', '--val_list', action='store', type="string", dest='val_list',
-                     help="val list's path", default="../tools/train_val_list/cs_val_list.txt")
+optParser.add_option('-t', '--data_split_type', action='store', type="string", dest='data_split_type',
+                     help="data_split_type", default="cv2")
 optParser.add_option('-m', '--pretrained_model', action='store', type="string", dest='pretrained_model',
-                     help="pretrained model's path", default="./cv1_Flow_num_seg7_dropout_08_flow_model_best.pth.tar")
-
+                     help="pretrained model's path", default="./cv2_Flow_num_seg7_dropout_08_flow_model_best.pth.tar")
+optParser.add_option('-g', '--gpus', action='append', type="int", dest='gpus',
+                     help="gpu to be used", default=[])
 option, args = optParser.parse_args()
-train_list = option.train_list
-val_list = option.val_list
+data_split_type = option.data_split_type
+train_list = "../tools/train_val_list/%s_train_list.txt" % data_split_type
+val_list = "../tools/train_val_list/%s_val_list.txt" % data_split_type
 pretrained_model = option.pretrained_model
+print(option.gpus)
+workers = len(option.gpus)
+
 num_segments = 7
 modality = "Flow"
 flow_prefix = "flow_"
@@ -37,11 +42,6 @@ batch_size = 1
 num_class = 12
 base_lr = 0.001
 dev = 2
-parser = argparse.ArgumentParser(
-    description="Standard video-level testing")
-parser.add_argument('--gpus', nargs='+', type=int, default=None)
-args = parser.parse_args()
-workers = len(args.gpus)
 
 net = TSN(num_class, num_segments, modality,
           base_model=arch,
@@ -53,8 +53,8 @@ base_dict = {'.'.join(k.split('.')[1:]): v for k, v in list(checkpoint['state_di
 net.load_state_dict(base_dict)
 print(net)
 print(net.input_size, net.scale_size, net.input_mean, net.input_std)
-print('model load done..cv1')
-workers = len(args.gpus)
+print('model load done..%s' % data_split_type)
+
 train_augmentation = net.get_augmentation()
 loader = []
 loader1 = torch.utils.data.DataLoader(
@@ -86,7 +86,7 @@ loader2 = torch.utils.data.DataLoader(
 loader.append(loader1)
 loader.append(loader2)
 val_data_gen = enumerate(loader2)
-devices = [args.gpus[i] for i in range(workers)]
+devices = [option.gpus[i] for i in range(workers)]
 net = torch.nn.DataParallel(net, device_ids=devices).cuda()
 net.eval()
 val_list = open(val_list).readlines()
@@ -98,7 +98,7 @@ for i, (data, label) in val_data_gen:
     feature = net(input_var)
     feature = feature.data.cpu().numpy().copy()
     feature = feature.reshape(1024)
-    out = p.replace('optical_flow_data', 'cv1_features')
+    out = p.replace('optical_flow_data', '%s_features' % data_split_type)
     if not os.path.exists(out):
         os.makedirs(out)
     np.save(os.path.join(out, 'TSN-feature.npy'), feature)
@@ -111,8 +111,8 @@ for i, (data, label) in data_gen:
     feature = net(input_var)
     feature = feature.data.cpu().numpy().copy()
     feature = feature.reshape(1024)
-    out = p.replace('optical_flow_data', 'cv1_features')
+    out = p.replace('optical_flow_data', '%s_features' % data_split_type)
     if not os.path.exists(out):
         os.makedirs(out)
     np.save(os.path.join(out, 'TSN-feature.npy'), feature)
-print('cv1')
+print(data_split_type)
